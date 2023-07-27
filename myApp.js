611a57fc47fd11e8890e319885fe48258c647f96
Elisa-602 <82344814+Elisa-602@users.noreply.github.com>
2023-07-27 11:24:10 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,50 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config();
 
+/*
+Start a Working Express Server
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
+*/
+
+//Serve an HTML File
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//Serve Static Assets
+app.use("/public", express.static(__dirname + "/public"));
+
+/*Serve JSON on a Specific Route
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"})
+})*/
+
+
+//Use the .env File
+/*app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"HELLO JSON"});
+  }
+  else {
+    res.json({"Hello json"});
+  }
+})*/
+
+var message= 'Hello json';
+app.get("/json", (req, res) => {
+  if ( process.env['MESSAGE_STYLE'] === "uppercase") {
+      res.json({ "message": message.toUpperCase() });
+  }
+  else {
+    res.json({ "message": messange });
+  }
+});
+
+console.log("Hello World");
 
 
 
